0870e65c123935bfa28effdf976a317720b431b6
Nathan Headley <nathanheadley1@gmail.com>
2016-04-12 15:50:24 +0100

    Added Player and GameObject classes

---
+++ app/src/main/java/zelphinstudios/courseworkapp/game/Button.java
@@ -0,0 +1,72 @@
+package zelphinstudios.courseworkapp.game;
+
+import android.graphics.Bitmap;
+
+public class Button {
+	
+	// Variables
+	private int x, y, width, height, actionId;
+	private Bitmap bitmap;
+	private boolean visible;
+	
+	
+	// Constructor
+	public Button (int x_, int y_, int width_, int height_,
+		int actionId_, Bitmap bitmap_, boolean visible_) {
+		x = x_;
+		y = y_;
+		width = width_;
+		height = height_;
+		actionId = actionId_;
+		bitmap = bitmap_;
+		visible = visible_;
+	}
+
+	
+	// Getters
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public int getActionId() {
+		return actionId;
+	}
+	public Bitmap getBitmap() {
+		return bitmap;
+	}
+	public boolean isVisible() {
+		return visible;
+	}
+	
+	
+	// Setters
+	public void setX(int x_) {
+		x = x_;
+	}
+	public void setY(int y_) {
+		y = y_;
+	}
+	public void setWidth(int width_) {
+		width = width_;
+	}
+	public void setHeight(int height_) {
+		height = height_;
+	}
+	public void setActionId(int actionId_) {
+		actionId = actionId_;
+	}
+	public void setBitmap(Bitmap bitmap_) {
+		bitmap = bitmap_;
+	}
+	public void setVisible(boolean visible_) {
+		visible = visible_;
+	}
+}
---
+++ app/src/main/java/zelphinstudios/courseworkapp/game/GameObject.java
@@ -0,0 +1,72 @@
+package zelphinstudios.courseworkapp.game;
+
+import android.graphics.Bitmap;
+
+public class GameObject {
+	
+	// Variables
+	private int x, y, width, height;
+	private Bitmap bitmap;
+	private boolean visible, solid;
+	
+	
+	// Constructor
+	public GameObject(int x_, int y_, int width_, int height_,
+		Bitmap bitmap_, boolean visible_, boolean solid_) {
+		x = x_;
+		y = y_;
+		width = width_;
+		height = height_;
+		bitmap = bitmap_;
+		visible = visible_;
+		solid = solid_;
+	}
+	
+	
+	// Getters
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public Bitmap getBitmap() {
+		return bitmap;
+	}
+	public boolean isVisible() {
+		return visible;
+	}
+	public boolean isSolid() {
+		return solid;
+	}
+	
+	
+	// Setters
+	public void setX(int x_) {
+		x = x_;
+	}
+	public void setY(int y_) {
+		y = y_;
+	}
+	public void setWidth(int width_) {
+		width = width_;
+	}
+	public void setHeight(int height_) {
+		height = height_;
+	}
+	public void setBitmap(Bitmap bitmap_) {
+		bitmap = bitmap_;
+	}
+	public void setVisible(boolean visible_) {
+		visible = visible_;
+	}
+	public void setSolid(boolean solid_) {
+		solid = solid_;
+	}
+}
