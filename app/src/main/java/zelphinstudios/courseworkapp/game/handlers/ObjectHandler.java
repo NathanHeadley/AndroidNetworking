c4141f06faa096bb7b7d79f05def4b83ab5d1be2
Nathan Headley <nathanheadley1@gmail.com>
2016-04-14 22:54:20 +0100

    Fixed crash bug. GameView displaying all content except player two.

--- app/src/main/java/zelphinstudios/courseworkapp/game/handlers/ObjectHandler.java
+++ app/src/main/java/zelphinstudios/courseworkapp/game/handlers/ObjectHandler.java
@@ -12,17 +12,22 @@ public class ObjectHandler {
 	// Variables
 	private Vector<GameObject> gameObjects = new Vector<>();
 	private BitmapDecoder bitmapDecoder;
+	private GameObject backgroundObject;
 
-
+	
 	// Constructor
 	public ObjectHandler(Context context_) {
 		bitmapDecoder = new BitmapDecoder(context_);
+		backgroundObject = new GameObject(0, 0, 32, 32, bitmapDecoder.decode("background"), true, true);
 		gameObjects.addElement(new GameObject(0, 0, 32, 32, bitmapDecoder.decode("boulder"), true, true));
 		gameObjects.addElement(new GameObject(50, 50, 32, 32, bitmapDecoder.decode("boulder"), true, true));
 	}
 
 	
 	// Getters
+	public GameObject getBackground() {
+		return backgroundObject;
+	}
 	public GameObject getGameObject(int object_) {
 		return gameObjects.get(object_);
 	}
--- app/src/main/java/zelphinstudios/courseworkapp/views/GameView.java
+++ app/src/main/java/zelphinstudios/courseworkapp/views/GameView.java
@@ -7,7 +7,9 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import zelphinstudios.courseworkapp.game.handlers.ObjectHandler;
+import zelphinstudios.courseworkapp.game.instances.Player;
 import zelphinstudios.courseworkapp.game.instances.GameObject;
+import android.graphics.*;
 
 public class GameView extends SurfaceView implements Runnable {
 
@@ -17,15 +19,17 @@ public class GameView extends SurfaceView implements Runnable {
     private boolean running = false;
 
     private ObjectHandler objectHandler;
+	private Player player;
 
 	/*public GameView(Context context_) {
 		super(context_);
 	}*/
 
-    public GameView(Context context_, ObjectHandler objectHandler_) {
+    public GameView(Context context_, ObjectHandler objectHandler_, Player player_) {
         super(context_);
         surfaceHolder = getHolder();
         objectHandler = objectHandler_;
+		player = player_;
     }
 
     public void run() {
@@ -35,13 +39,24 @@ public class GameView extends SurfaceView implements Runnable {
             }
             Canvas canvas = surfaceHolder.lockCanvas();
 
+			if(objectHandler.getBackground().getBitmap() != null) {
+				for(int y = 0; y < 5; y++) {
+					for(int x = 0; x < 5; x++) {
+						canvas.drawBitmap(objectHandler.getBackground().getBitmap(), 96*x, 96*y, null);
+					}
+				}
+			}
+			
             for(GameObject object : objectHandler.getGameObjects()) {
-	            Log.e("Nathan", ""+object.getBitmap().toString());
 	            if(object.getBitmap() != null) {
 		            canvas.drawBitmap(object.getBitmap(), object.getX(), object.getY(), null);
 	            }
             }
 
+			if(player.getBitmap() != null) {
+				canvas.drawBitmap(player.getBitmap(), player.getX(), player.getY(), null);
+			}
+				
             surfaceHolder.unlockCanvasAndPost(canvas);
         }
     }
