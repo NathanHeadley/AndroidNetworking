ca1614c5cdaccb717b6d9b830e5818a89ffad6c7
NathanHeadley <Nathanheadley1@gmail.com>
2016-04-15 09:21:47 +0100

    Landscape mode testing.

--- app/src/main/java/zelphinstudios/courseworkapp/views/GameView.java
+++ app/src/main/java/zelphinstudios/courseworkapp/views/GameView.java
@@ -14,6 +14,7 @@ import android.graphics.*;
 public class GameView extends SurfaceView implements Runnable {
 
     // Threading
+	private Context context;
     private Thread thread = null;
     private SurfaceHolder surfaceHolder;
     private boolean running = false;
@@ -21,12 +22,14 @@ public class GameView extends SurfaceView implements Runnable {
     private ObjectHandler objectHandler;
 	private Player player;
 
-	/*public GameView(Context context_) {
+	public GameView(Context context_) {
 		super(context_);
-	}*/
+		context = context_;
+	}
 
     public GameView(Context context_, ObjectHandler objectHandler_, Player player_) {
         super(context_);
+	    context = context_;
         surfaceHolder = getHolder();
         objectHandler = objectHandler_;
 		player = player_;
@@ -39,23 +42,28 @@ public class GameView extends SurfaceView implements Runnable {
             }
             Canvas canvas = surfaceHolder.lockCanvas();
 
-			if(objectHandler.getBackground().getBitmap() != null) {
-				for(int y = 0; y < 5; y++) {
-					for(int x = 0; x < 5; x++) {
-						canvas.drawBitmap(objectHandler.getBackground().getBitmap(), 96*x, 96*y, null);
-					}
-				}
-			}
-			
-            for(GameObject object : objectHandler.getGameObjects()) {
-	            if(object.getBitmap() != null) {
-		            canvas.drawBitmap(object.getBitmap(), object.getX(), object.getY(), null);
-	            }
-            }
+	        if(context.getResources().getConfiguration().orientation == 1) { //1 = portrait, 2 = landscape
+		        if (objectHandler.getBackground().getBitmap() != null) {
+			        for (int y = 0; y < 5; y++) {
+				        for (int x = 0; x < 5; x++) {
+					        canvas.drawBitmap(objectHandler.getBackground().getBitmap(), 96 * x, 96 * y, null);
+				        }
+			        }
+		        }
 
-			if(player.getBitmap() != null) {
-				canvas.drawBitmap(player.getBitmap(), player.getX(), player.getY(), null);
-			}
+		        for (GameObject object : objectHandler.getGameObjects()) {
+			        if (object.getBitmap() != null) {
+				        canvas.drawBitmap(object.getBitmap(), object.getX(), object.getY(), null);
+			        }
+		        }
+
+		        if (player.getBitmap() != null) {
+			        canvas.drawBitmap(player.getBitmap(), player.getX(), player.getY(), null);
+		        }
+	        } else if(context.getResources().getConfiguration().orientation == 2) {
+		        //landscape
+		        Log.e("Nathan", "I'm in Landscape mode!");
+	        }
 				
             surfaceHolder.unlockCanvasAndPost(canvas);
         }
